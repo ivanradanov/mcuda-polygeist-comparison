ac0da890cb5727164cbb0344908a91898c515fce
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2022-04-01 07:50:10 +0900

    fix

--- run-scaling.sh
+++ run-scaling.sh
@@ -3,14 +3,26 @@
 
 DIMS="$1"
 THREADS="$2"
+NRUNS="$3"
 
 EXES="./mcuda/a.out ./polygeist/a.out"
 
+echo mcuda_results = \{
 for EXE in $EXES; do
-for DIM in $DIMS; do
-    for THREAD in $THREADS; do
-        echo OMP_THREAD_NUM="$THREAD" taskset -c 0-$(($THREAD-1)) numactl -N 0 -m 0 "$EXE" -wA=$DIM -wB=$DIM -hA=$DIM -hB=$DIM
+    echo \"$EXE\"\: \{
+    for DIM in $DIMS; do
+        echo $DIM\: \{
+        for THREAD in $THREADS; do
+            echo $THREAD\:
+            echo -n \[
+            for i in $(seq $NRUNS); do
+                >&2 echo "$EXE" $DIM "$THREAD" $i
+                OMP_NUM_THREADS="$THREAD" taskset -c 0-$(($THREAD-1)) numactl -N 0 -m 0 "$EXE" -wA=$DIM -wB=$DIM -hA=$DIM -hB=$DIM
+            done
+            echo \],
+        done
+        echo \},
     done
+    echo \},
 done
-done
-
+echo \}
